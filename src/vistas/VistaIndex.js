64eaaf49b56b889aecd04e75cf0b39f83e400151
WilliamPerezBeltran <williampbeltranprogramador@gmail.com>
2024-08-04 11:25:26 -0500

    implement view and router

---
+++ src/vistas/VistaIndex.js
@@ -0,0 +1,13 @@
+import React, { Component, lazy, Suspense } from "react";
+
+function VistaIndex() {
+  
+  return (
+    <div>
+    <p>vista index</p>
+      
+    </div>
+  );
+}
+
+export default VistaIndex;
